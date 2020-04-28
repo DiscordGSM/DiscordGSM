39203e4f51854bf3b5dd8a6f9777146c20ae90dc
Battlefield Duck <hkdsethomas@gmail.com>
2020-04-28 16:11:59 +0800

    Fix GamedigQuery not working
    Fix `AttributeError: 'bytes' object has no attribute 'read'` on `stdout.read()`

--- bin/GamedigQuery.py
+++ bin/GamedigQuery.py
@@ -13,7 +13,7 @@ class GamedigQuery(object):
             process = subprocess.run(
                 ['gamedig', '--type', str(self.game), '--host', str(self.ip), '--port', str(self.port)],
                 stdout=subprocess.PIPE)
-            output = process.stdout.read()
+            output = process.stdout.decode("utf-8")
 
             json_reader = json.loads(str(output).replace("b'", "").replace("\\n'", ""))
 
