9f78bf2374c5c386304f903c5847724b08181f75
Battlefield Duck <hkdsethomas@gmail.com>
2020-10-26 22:11:44 +0800

    Update app.json, app.js

--- heroku/app.js
+++ heroku/app.js
@@ -1,7 +1,7 @@
 const debugPort = 7999
 const heroku = process.env.PORT != null
 const express = require('express')
-const http = require('http')
+const https = require('https')
 const path = require('path')
 const fs = require('fs')
 const version = new RegExp("VERSION.{0,}=.{0,}'(.*)'").exec(fs.readFileSync(path.join(__dirname, '/../bot.py')))[1] // Get DiscordGSM verison from bot.py
@@ -9,7 +9,7 @@ const serversJson = heroku ? process.env.SERVERS_JSON : fs.readFileSync(path.joi
 const inviteLink = require('child_process').execSync(`${(heroku ? 'python3 ' : '')}getbotinvitelink.py`, { encoding : 'utf8' }) // Get bot invite link by getbotinvitelink.py
 
 const app = express()
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/public')))
 app.use('/', (_, res) => {
@@ -26,12 +26,12 @@ app.use('/', (_, res) => {
         inviteLink: inviteLink,
         SERVERS_JSON: JsonValid ? '✔️' : '❌',
         DGSM_TOKEN: inviteLink.includes('https://discordapp.com/api/oauth2/authorize?client_id=') ? '✔️' : '❌',
-    });
+    })
 })
 app.listen(heroku ? process.env.PORT : debugPort)
 
 if (process.env.HEROKU_APP_NAME != null) {
-    setInterval(() => http.get(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com?t=${+new Date()}`), 300000)
+    setInterval(() => https.get(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com?t=${+new Date()}`), 5 * 60 * 1000)
 } else {
     console.log('ERROR: HEROKU_APP_NAME Config Var not found')
 }
